fd905242bf5bfb4b7a4b78aeeef99771b450325a
Amin <amin04.edu@gmail.com>
2026-07-31 08:55:57 +0500

    synchronization in parallel programming

---
+++ Lab05task02.py
@@ -0,0 +1,4 @@
+# Q2: Why is synchronization important in parallel programming?
+
+# Parallel processes run independently and at different speeds.
+# Without synchronization it will cause race conditions, deadlocks, incorrect results, or non-deterministic behavior.
